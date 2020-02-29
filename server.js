2f5e755c529aa8c981ec8be5e833a40d7255076c
tamotsu tanabe <tamotsu@sightlineapplications.com>
2020-02-28 22:51:17 -0800

    Workingn on deployment...

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
