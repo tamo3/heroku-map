cc2009a1b3f229ee63726c0d8ee8c1435579b1e7
tamotsu tanabe <tamotsu@sightlineapplications.com>
2020-02-29 14:06:37 -0800

    Adding map.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
+
+const mapKey = 'AIzaSyBdBb3UJJAFYHU76vXhd_sBoSdvVJk6WaI';
+// class MyMap extends Component {
+//   render() {
+//     return (
+//       <div className="MyMap">
+//         Hello World!
+//         <Map google={this.props.google} />
+//       </div>
+//     )
+//   }
+// }
 
 class App extends Component {
   state = {
@@ -30,11 +43,15 @@ class App extends Component {
         </header>
         <h3>Here is what returned from Express server: </h3>
         <p className="App-intro">{this.state.data}</p>
+        <Map google={this.props.google} />
         <div id="debug-div">(for debugging)</div>
       </div>
     );
   }
 }
   
-export default App;
+// export default App;
+export default GoogleApiWrapper({
+  apiKey: (mapKey)
+ })(App);
 
