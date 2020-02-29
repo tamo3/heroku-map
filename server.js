72a178440fce27b4cb66824d2be129bf3e6fb437
tamotsu tanabe <tamotsu@sightlineapplications.com>
2020-02-28 21:47:15 -0800

    Adding react frontend.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-var port = process.env.PORT || 8080;
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.use(express.static(__dirname));
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  console.log('server app.get called');
+  res.send({ "express": "Hello from Express!" });
+});
 
-app.get("/", function (req, res) {
-  res.render("index");
-})
 
-app.listen(port, function () {
-  console.log("app running");
-})
