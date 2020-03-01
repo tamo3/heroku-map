a4ce63c474eb677dc6be7647f856f7e857b6507c
tamotsu tanabe <tamotsu@sightlineapplications.com>
2020-02-29 22:07:50 -0800

    Trying MongoDB.

--- server.js
+++ server.js
@@ -1,9 +1,107 @@
 // Reference: https://medium.com/@maison.moa/setting-up-an-express-backend-server-for-create-react-app-bc7620b20a61
-
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;  // Use given port when deployed, or localhost:5000.
+
+
+// Referense: https://github.com/mongolab/mongodb-driver-examples/blob/master/nodejs/mongooseSimpleExample.js
+const dbUrl = 'mongodb://heroku_tvsq48kq:7rv7942mg8365kipoj8a8rad2i@ds139342.mlab.com:39342/heroku_tvsq48kq';
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback() {
+
+  // Create song schema
+  let songSchema = mongoose.Schema({
+    decade: String,
+    artist: String,
+    song: String,
+    weeksAtOne: Number
+  });
+
+  // Store song documents in a collection called "songs"
+  let Song = mongoose.model('songs', songSchema);
+
+  // Create seed data
+  let seventies = new Song({
+    decade: '1970s',
+    artist: 'Debby Boone',
+    song: 'You Light Up My Life',
+    weeksAtOne: 10
+  });
+
+  let eighties = new Song({
+    decade: '1980s',
+    artist: 'Olivia Newton-John',
+    song: 'Physical',
+    weeksAtOne: 10
+  });
+
+  let nineties = new Song({
+    decade: '1990s',
+    artist: 'Mariah Carey',
+    song: 'One Sweet Day',
+    weeksAtOne: 16
+  });
+
+  /*
+   * First we'll add a few songs. Nothing is required to create the
+   * songs collection; it is created automatically when we insert.
+   */
+
+  let list = [seventies, eighties, nineties]
+
+  Song.insertMany(list).then(() => {
+
+    /*
+     * Then we need to give Boyz II Men credit for their contribution
+     * to the hit "One Sweet Day".
+     */
+
+    return Song.update({ song: 'One Sweet Day'}, { $set: { artist: 'Mariah Carey ft. Boyz II Men'} })
+
+  }).then(() => {
+
+    /*
+     * Finally we run a query which returns all the hits that spend 10 or
+     * more weeks at number 1.
+     */
+
+    return Song.find({ weeksAtOne: { $gte: 10} }).sort({ decade: 1})
+
+  }).then(docs => {
+
+    docs.forEach(doc => {
+      console.log(
+        'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
+        ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
+      );
+    });
+
+  }).then(() => {
+
+    // Since this is an example, we'll clean up after ourselves.
+    return mongoose.connection.db.collection('songs').drop()
+
+  }).then(() => {
+
+    // Only close the connection when your app is terminating
+    return mongoose.connection.close()
+
+  }).catch(err => {
+
+    // Log any errors that are thrown in the Promise chain
+    console.log(err)
+
+  })
+});
+
+
+
 
 // app.use(express.static(path.join(__dirname, 'client/build')));
 console.log('PUBLIC_URL is: ', process.env.PUBLIC_URL);
