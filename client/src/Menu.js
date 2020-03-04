ee366f72c23e3a4c37c1cb6f5fd94d8785294c78
mdszn <dmirko@pdx.edu>
2020-03-03 19:55:25 -0800

    added bootstrap to menu component

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import MapContainer from './Map.js';
 
 
+
+
 class Menu extends Component {
+
   getData(cbGetData) {
     cbGetData(); // Call callback function.
   }
@@ -13,11 +17,13 @@ class Menu extends Component {
 
   render() {
     return (
-      <div>
-        <button type="button" onClick={() => this.getData(this.props.cbGetData)} title="get data from DB">Debug Get Events</button>
-        <button type="button" onClick={() => this.addData(this.props.cbAddData)} title="add data to DB">Debug Add Event</button>
-        <p id="debug"></p>
-      </div>
+    <div>
+      <button type="button" class="dash-button btn btn-primary mb-1" onClick={() => this.getData(this.props.cbGetData)} title="get data from DB">Debug Get Events</button>
+      <button type="button" class="dash-button btn btn-primary" onClick={() => this.addData(this.props.cbAddData)} title="add data to DB">Debug Add Event</button>
+      <p id="debug"></p>
+    </div>
+
+
     )
   }
 }
